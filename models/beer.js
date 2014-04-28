40978e9da8542e845306d02edfc59125b8762d79
joao.sobotka <joao.sobotka@CCSDESDSK026.consistem.local>
2014-04-28 10:24:47 -0300

    First commit

---
+++ models/beer.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/be_mean');
+
+var db = mongoose.connection;
+db.on('error', function(err){
+    console.log('Erro de conexao.', err)
+});
+db.once('open', function () {
+  console.log('Conexão aberta.')
+});
+
+var Schema = mongoose.Schema;
+
+var BeerSchema = new Schema({
+  name: { type: String, default: '' },
+  description: { type: String, default: '' },
+  alcohol: { type: Number, min: 0 },
+  style: { type: String, default: '' },
+  brewery: { type: String, default: '' },
+  category: { type: String, default: '' },
+  country: { type: String, default: '' },
+  created: { type: Date, default: Date.now }
+});
+
+exports.model = mongoose.model('Beer', BeerSchema);
